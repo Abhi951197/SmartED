7a9eb05b13be7dc9425ac195dd848a2e2373847d
Abhishek Pal <128469413+Abhi951197@users.noreply.github.com>
2025-04-23 13:22:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.secret_key = 'your_secret_key_here'  # Change this to a random secret key
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
